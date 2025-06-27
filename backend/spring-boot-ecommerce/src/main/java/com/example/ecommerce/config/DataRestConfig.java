9a62f995f751a8283bd366bac41ca5db3a67745a
kyleradcliffeong <kyle.ong00@gmail.com>
2025-06-27 11:09:38 +0800

    Added jpa repository method to search products by category. Also, changed the config to expose id field in rest json response

--- backend/spring-boot-ecommerce/src/main/java/com/example/ecommerce/config/DataRestConfig.java
+++ backend/spring-boot-ecommerce/src/main/java/com/example/ecommerce/config/DataRestConfig.java
@@ -2,14 +2,28 @@ package com.example.ecommerce.config;
 
 import com.example.ecommerce.entity.Product;
 import com.example.ecommerce.entity.ProductCategory;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.metamodel.EntityType;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
 @Configuration
 public class DataRestConfig implements RepositoryRestConfigurer {
+
+    private EntityManager entityManager;
+
+    @Autowired
+    public DataRestConfig(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
 
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
@@ -26,5 +40,19 @@ public class DataRestConfig implements RepositoryRestConfigurer {
                 .forDomainType(ProductCategory.class)
                 .withItemExposure((metadata, httpMethods) -> httpMethods.disable(disabledActions))
                 .withCollectionExposure((metadata, httpMethods) -> httpMethods.disable(disabledActions));
+
+        // call helper method to expose id field in rest json response
+        exposeIds(config);
+    }
+
+    private void exposeIds(RepositoryRestConfiguration config) {
+        Set<EntityType<?>> entities = entityManager.getMetamodel().getEntities();
+        List<Class> entityClasses = new ArrayList<>();
+        for (EntityType entityType: entities) {
+            entityClasses.add(entityType.getJavaType());
+        }
+        // convert the ArrayList to an array of Class objects
+        Class[] domainTypes = entityClasses.toArray(new Class[0]);
+        config.exposeIdsFor(domainTypes);
     }
 }
